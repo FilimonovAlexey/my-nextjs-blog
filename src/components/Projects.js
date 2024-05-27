9656f920c064621ebdc2b1fb3d6f4d4380fccf91
Alexey <alexeyf08@gmail.com>
2024-05-27 22:06:50 +0700

    Добавлены ссылки на мои проекты

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,13 +4,22 @@ export default function Projects() {
       <h2>Мои проекты</h2>
       <ul>
         <li>
-          <strong>Telegram бот Предложка</strong>: Telegram бот Предложка.
+          <strong>Telegram бот Предложка</strong>: 
+          <a href="https://github.com/FilimonovAlexey/YouTubeHelperTgBot" target="_blank" rel="noopener noreferrer">
+            Telegram бот Предложка.
+          </a>
         </li>
         <li>
-          <strong>Аналог Taplink на React JS</strong>: Страница со всеми социальными сетями.
+          <strong>Аналог Taplink на React JS</strong>: 
+          <a href="https://github.com/FilimonovAlexey/link-bio-page" target="_blank" rel="noopener noreferrer">
+            Страница со всеми социальными сетями.
+          </a>
         </li>
         <li>
-          <strong>Репозиторий на GitHub</strong>: Полезные материалы по веб-разработке!
+          <strong>Репозиторий на GitHub</strong>: 
+          <a href="https://github.com/FilimonovAlexey/awesome-frontend-dev" target="_blank" rel="noopener noreferrer">
+            Полезные материалы по веб-разработке!
+          </a>
         </li>
       </ul>
     </section>
