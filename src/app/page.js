2d990a8a27498859572bd34eccef563d5cafd9d0
Alexey <alexeyf08@gmail.com>
2024-06-02 12:32:24 +0700

    Добавлены иконки социальных сетей

--- src/app/page.js
+++ src/app/page.js
@@ -9,12 +9,26 @@ export default async function Home() {
     <div>
       <section>
         <p>Алексей. Занимаюсь frontend-разработкой и пишу SQL скрипты в одной IT компании, веду канал на YouTube. На развитие канала автору!</p>
-        <p>Я в социальных сетях:</p>
-        <ul>
-          <li><a href="#">Telegram</a></li>
-          <li><a href="#">YouTube</a></li>
-          <li><a href="#">GitHub</a></li>
-        </ul>
+        <p style={{ display: 'flex', alignItems: 'center' }}>
+          Я в социальных сетях:
+          <ul className="social-icons" style={{ marginLeft: '10px' }}>
+            <li>
+              <Link href="https://www.youtube.com/" target="_blank">
+                <img src="/icons/youtube.svg" alt="YouTube" />
+              </Link>
+            </li>
+            <li>
+              <Link href="https://telegram.org/" target="_blank">
+                <img src="/icons/telegram.svg" alt="Telegram" />
+              </Link>
+            </li>
+            <li>
+              <Link href="https://github.com/" target="_blank">
+                <img src="/icons/github.svg" alt="GitHub" />
+              </Link>
+            </li>
+          </ul>
+        </p>
       </section>
       <h1>Статьи</h1>
       <ul>
